4ffe1038c718139edee6955665e1de9e1480972b
kadraknb <kadrak02@hotmail.com>
2022-07-21 08:55:43 -0300

    Requisitos 6,7

--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -1,1 +1,55 @@
-test('', () => {});
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import React from 'react';
+import App from '../App';
+import renderWithRouter from './resertaHistorico';
+
+const MoreDetails = 'More details';
+const pokemons25 = 'pokemons/25';
+describe('Teste o componente <Pokemon.js />', () => {
+  test('se é renderizado um card com as informações de determinado pokémon:', () => {
+    const { history } = renderWithRouter(<App />);
+    history.push('/');
+    userEvent.click(screen.getAllByRole('button')[1]);
+    expect(screen.getByTestId('pokemon-name').innerHTML).toBe('Pikachu');
+    expect(screen.getByTestId('pokemon-type').innerHTML).toBe('Electric');
+    expect(screen.getByTestId('pokemon-weight').innerHTML).toBe('Average weight: 6.0 kg');
+    const imgP = screen.getByRole('img');
+    expect(imgP.src).toBe('https://cdn2.bulbagarden.net/upload/b/b2/Spr_5b_025_m.png');
+    expect(imgP.alt).toBe('Pikachu sprite');
+  });
+
+  test('Teste se o card do pokémon indicado na Pokédex contém um link ', () => {
+    const { history } = renderWithRouter(<App />);
+    history.push('/');
+    userEvent.click(screen.getAllByRole('button')[1]);
+    expect(screen.getByRole('link', { name: MoreDetails }).href).toBe('http://localhost/pokemons/25');
+  });
+  test('Teste se ao clicar no link de navegação do pokémon', () => {
+    const { history } = renderWithRouter(<App />);
+    history.push('/');
+    userEvent.click(screen.getAllByRole('button')[1]);
+    const pika = screen.getByTestId('pokemon-name').innerHTML;
+    userEvent.click(screen.getByRole('link', { name: MoreDetails }));
+    expect(pika).toBe('Pikachu');
+    const { location: { pathname } } = history;
+    expect(pathname).toBe('/pokemons/25');
+  });
+  test('Teste também se a URL exibida no navegador muda', () => {
+    const { history } = renderWithRouter(<App />);
+    history.push('/');
+    userEvent.click(screen.getAllByRole('button')[1]);
+    expect(screen.getByRole('link', { name: MoreDetails }).href).toBe('http://localhost/pokemons/25');
+    userEvent.click(screen.getByRole('link', { name: MoreDetails }));
+    const { location: { pathname } } = history;
+    expect(pathname).toBe('/pokemons/25');
+  });
+  test('Teste se existe um ícone de estrela nos pokémons favoritados', () => {
+    const { history } = renderWithRouter(<App />);
+    history.push(pokemons25);
+    userEvent.click(screen.getByRole('checkbox'));
+    const img = screen.getByRole('img', { name: 'Pikachu is marked as favorite' });
+    expect(img.src).toBe('http://localhost/star-icon.svg');
+    expect(img.alt).toBe('Pikachu is marked as favorite');
+  });
+});
--- src/tests/PokemonDetails.test.js
+++ src/tests/PokemonDetails.test.js
@@ -1,1 +1,52 @@
-test('', () => {});
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import React from 'react';
+import App from '../App';
+import renderWithRouter from './resertaHistorico';
+
+describe('Teste o componente <PokemonDetails.js />', () => {
+  test('se informações detalhadas do pokémon selecionado são mostradas na tela', () => {
+    const { history } = renderWithRouter(<App />);
+    history.push('/pokemons/25');
+
+    expect(screen.getByRole('heading', { name: 'Pikachu Details' })).toBeDefined();
+    screen.getAllByRole('link').forEach((aa) => {
+      expect(aa.href !== 'http://localhost/pokemons/25').toBe(true);
+    });
+    expect(screen.getByRole('heading', { name: 'Summary' }).innerHTML).toBe('Summary');
+    expect(screen.getByText(/This intelligent Pokémon/i)).toBeDefined();
+    expect(screen.getByTestId('pokemon-name').innerHTML).toBe('Pikachu');
+    expect(screen.getByTestId('pokemon-type').innerHTML).toBe('Electric');
+    expect(screen.getByTestId('pokemon-weight').innerHTML).toBe('Average weight: 6.0 kg');
+  });
+
+  test('se existe na página uma seção com os mapas', () => {
+    const { history } = renderWithRouter(<App />);
+    history.push('/pokemons/25');
+    const pikachuL = 'Pikachu location';
+    expect(screen.getByRole('heading', { name: /Game Locations of Pikachu/i }));
+    expect(screen.getAllByRole('img', { name: pikachuL }).length).toBe(2);
+    const img = screen.getAllByRole('img', { name: pikachuL });
+    expect(img[0].src).toBe('https://cdn2.bulbagarden.net/upload/0/08/Kanto_Route_2_Map.png');
+    expect(img[0].alt).toBe(pikachuL);
+    expect(img[1].src).toBe('https://cdn2.bulbagarden.net/upload/b/bd/Kanto_Celadon_City_Map.png');
+    expect(img[1].alt).toBe(pikachuL);
+    expect(screen.getByText('Kanto Viridian Forest')).toBeDefined();
+    expect(screen.getByText('Kanto Power Plant')).toBeDefined();
+  });
+
+  test('se o usuário pode favoritar um pokémon através da página de detalhes', () => {
+    const { history } = renderWithRouter(<App />);
+    history.push('/pokemons/148');
+
+    const pokemonF = screen.getByRole('checkbox');
+    expect(pokemonF).toBeDefined();
+    expect(screen.getByLabelText('Pokémon favoritado?')).toBeDefined();
+    userEvent.click(pokemonF);
+    expect(screen.getByRole('img', { name: 'Dragonair is marked as favorite' }));
+    userEvent.click(pokemonF);
+    screen.getAllByRole('img').forEach((aa) => {
+      expect(aa.alt !== 'Dragonair is marked as favorite').toBe(true);
+    });
+  });
+});
